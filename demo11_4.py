9bedffec12794c8946b66bab1a1bfd1405f6562f
Shawn Hutchinson <shutch@ksu.edu>
2023-11-07 11:09:37 -0600

    Update demo11_4.py

--- demo11_4.py
+++ demo11_4.py
@@ -4,7 +4,7 @@
 #    File name: demo11_4.py
 #    Author: Shawn Hutchinson
 #    Description:  Implement an update cursor and geometry token
-#    Date created: 11/06/2023
+#    Date created: 11/07/2023
 #    Python Version: 3.9.16
 
 # Import required modules and classes:
@@ -19,7 +19,6 @@ env.overwriteOutput = True
 inFc = "kansas_wonders_1"
 field = "Name"
 value = "Kansas State University"
-##whereClause = "'Name' = 'Kansas State University'"
 whereClause = """{0} = '{1}'""".format(arcpy.AddFieldDelimiters(inFc, field), value)
 ptList = [[-96.583091, 39.1189715]]
 
@@ -28,5 +27,4 @@ with arcpy.da.UpdateCursor(inFc, "SHAPE@", whereClause) as cursor:
     for row in cursor:
         for pt in ptList:
             inPoint = arcpy.Point(pt[0], pt[1])
-            cursor.updateRow((inPoint,))
-            ##cursor.updateRow((arcpy.Point(pt[0], pt[1]), ))       
+            cursor.updateRow(arcpy.PointGeometry(inPoint))
